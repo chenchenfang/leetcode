004a81508a6a64b584e7a1a440ebc0ccf87e2d7e
fang7 <857263385@qq.com>
2019-08-31 08:51:33 +0800

    更新了优化算法

---
+++ src/main/java/easy/IntergerInversionOptimizing.java
@@ -0,0 +1,29 @@
+package easy;
+
+public class IntergerInversionOptimizing {
+    static class Solution {
+        public int reverse(int x) {
+            int ans=0;
+            while (x!=0){
+                int pop=x%10;
+                if(ans>Integer.MAX_VALUE/10 ||(ans==Integer.MAX_VALUE/10 && pop>7)){
+                    return 0;
+                }
+                if(ans<Integer.MIN_VALUE/10 || (ans==Integer.MIN_VALUE/10 &&pop< -8)){
+                    return 0;
+                }
+                ans=ans*10+pop;
+                x/=10;
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        System.out.println(solution.reverse(1534236469));
+    }
+
+}
+
